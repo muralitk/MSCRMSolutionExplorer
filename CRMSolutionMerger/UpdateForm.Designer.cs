96a9322ca9c71160d802ed1d80eb2c698d6792aa
muralitk <murali.tk@gmail.com>
2020-03-31 14:19:58 +0800

    Add files via upload

---
+++ CRMSolutionMerger/UpdateForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CRMSolutionMerger
+{
+    partial class UpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbSolution = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSolution = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDisplayName = new System.Windows.Forms.TextBox();
+            this.txtVersion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.gbSolution.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbSolution
+            // 
+            this.gbSolution.Controls.Add(this.btnUpdate);
+            this.gbSolution.Controls.Add(this.btnCancel);
+            this.gbSolution.Controls.Add(this.txtVersion);
+            this.gbSolution.Controls.Add(this.label3);
+            this.gbSolution.Controls.Add(this.txtDisplayName);
+            this.gbSolution.Controls.Add(this.label2);
+            this.gbSolution.Controls.Add(this.lblSolution);
+            this.gbSolution.Controls.Add(this.label1);
+            this.gbSolution.Location = new System.Drawing.Point(5, 4);
+            this.gbSolution.Name = "gbSolution";
+            this.gbSolution.Size = new System.Drawing.Size(454, 146);
+            this.gbSolution.TabIndex = 0;
+            this.gbSolution.TabStop = false;
+            this.gbSolution.Text = "Solution Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Solution:";
+            // 
+            // lblSolution
+            // 
+            this.lblSolution.AutoSize = true;
+            this.lblSolution.ForeColor = System.Drawing.Color.Blue;
+            this.lblSolution.Location = new System.Drawing.Point(99, 20);
+            this.lblSolution.Name = "lblSolution";
+            this.lblSolution.Size = new System.Drawing.Size(48, 13);
+            this.lblSolution.TabIndex = 1;
+            this.lblSolution.Text = "Solution:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Display Name:";
+            // 
+            // txtDisplayName
+            // 
+            this.txtDisplayName.Location = new System.Drawing.Point(102, 42);
+            this.txtDisplayName.Name = "txtDisplayName";
+            this.txtDisplayName.Size = new System.Drawing.Size(339, 20);
+            this.txtDisplayName.TabIndex = 3;
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(102, 72);
+            this.txtVersion.Name = "txtVersion";
+            this.txtVersion.Size = new System.Drawing.Size(186, 20);
+            this.txtVersion.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Version:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(285, 109);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(366, 109);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "&Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // UpdateForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 157);
+            this.Controls.Add(this.gbSolution);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "UpdateForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Update Solution";
+            this.Load += new System.EventHandler(this.UpdateForm_Load);
+            this.gbSolution.ResumeLayout(false);
+            this.gbSolution.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSolution;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSolution;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVersion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDisplayName;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
